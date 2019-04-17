631befc23ef58b983dab88f6ba08d0b57fa5557a
bilalbaahmad <bilala@sigmatec.com.pk>
2019-04-17 10:15:02 +0500

    17-4-19

---
+++ database/migrations/2019_04_17_050053_create_file_folder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileFolder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_folder', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('parent');
+            $table->string('text');
+            $table->unsignedInteger('type');
+            $table->string('icon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_folder');
+    }
+}
